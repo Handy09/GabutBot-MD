62c8cb8e280049c5a0b7aa34e936a51472053f15
Handy09 <157579395+Handy09@users.noreply.github.com>
2024-02-03 21:29:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,10 +8,10 @@ global.namabot = 'ɢᴀʙᴜᴛʙᴏᴛ-ʀᴇʙᴏʀɴ'
 global.namaowner = 'ShinySebastian'
 
 //—————「 Setting Owner 」—————//
-global.owner = '628116646665'
-global.nomorlu = '6281932309239'
-global.ownernomer = ["628116646665"]
-global.premium = ['628116646665']
+global.owner = '62882006994648'
+global.nomorlu = '62882006994648'
+global.ownernomer = ["62882006994648"]
+global.premium = ['62882006994648']
 
 //—————「 Set Wm 」—————//
 global.packname = 'GabutBot-Reborn'
